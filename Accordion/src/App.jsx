e8a3d67eae439125ec4c6d2a84671184577bdfec
Avtex19 <atukaberadze19@gmail.com>
2024-09-21 14:28:47 +0400

    initialized project

---
+++ Accordion/src/App.jsx
@@ -0,0 +1,15 @@
+import AccordionCard from "./components/AccordionCard.jsx";
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <AccordionCard/>
+            </div>
+        </>
+    );
+}
+
+export default App;
+
---
+++ Accordion/src/components/AccordionCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './AccordionCard.module.css'
+
+function AccordionCard() {
+    return (
+        <div className={styles.cardContainer}>AccordionCard</div>
+    )
+}
+
+export default AccordionCard

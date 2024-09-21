43a36475835fd4059faefeae1718743fec60ef77
Avtex19 <atukaberadze19@gmail.com>
2024-09-21 16:27:35 +0400

    added accordeon functionality and structured components

--- Accordion/src/components/AccordionCard.jsx
+++ Accordion/src/components/AccordionCard.jsx
@@ -2,9 +2,32 @@ import React, {useState} from 'react'
 import styles from './AccordionCard.module.css'
 import AccordionItem from "./AccordionItem.jsx";
 
+const items = [
+    {
+        itemNumber: 1,
+        title: "Accordion1 Title 1",
+        content: '11111111',
+    },
+    {
+        itemNumber: 2,
+        title: "Accordion1 Title 2",
+        content: '22222222',
+    },
+    {
+        itemNumber: 3,
+        title: "Accordion1 Title 3",
+        content: '33333333',
+    },
+    {
+        itemNumber: 4,
+        title: "Accordion1 Title 4",
+        content: '44444444',
+    }
+]
 
 function AccordionCard() {
     const [activeAccordionNum, setActiveAccordionNum] = useState(null);
+
     const handleClick = (accordionNumber) => {
         if (activeAccordionNum === accordionNumber) {
             setActiveAccordionNum(null);
@@ -17,23 +40,11 @@ function AccordionCard() {
 
 
     return (<div className={styles.cardContainer}>
-        <AccordionItem handleClick={handleClick} activeAccordionNum={activeAccordionNum} itemNumber={4}
-                       title="Accordion1 Title"
-                       content="111111111"/>
-
-        <AccordionItem handleClick={handleClick} activeAccordionNum={activeAccordionNum} itemNumber={4}
-                       title="Accordion2 Title"
-                       content="22222222"/>
-
-        <AccordionItem handleClick={handleClick} activeAccordionNum={activeAccordionNum} itemNumber={4}
-                       title="Accordion3 Title"
-                       content="33333333"/>
-
-        <AccordionItem handleClick={handleClick} activeAccordionNum={activeAccordionNum} itemNumber={4}
-                       title="Accordion4 Title"
-                       content="444444444444"/>
-
-
+        {items.map((item) => {
+            return (<AccordionItem handleClick={handleClick} activeAccordionNum={activeAccordionNum} itemNumber={item.itemNumber}
+                                   title={item.title}
+                                   content={item.content}/>)
+        })}
     </div>)
 }
 
